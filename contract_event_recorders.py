b4de993080d85831a886baa88a3712ffa2e39ba5
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-01-22 00:28:13 +0300

    Clean up contract event recorders a bit

--- contract_event_recorders.py
+++ contract_event_recorders.py
@@ -1,72 +1,92 @@
 from app import App
-from contract_event_utils import block_timestamp
+from contract_event_utils import block_timestamp, coerce_to_int, parse_insert_status
 from datetime import datetime, timezone
 import logging
 from pprint import pprint
 from web3 import Web3
 
+logger = logging.getLogger("contract_event_recorders")
+
+INSERT_TRADE_STMT = """
+    INSERT INTO trades
+    (
+        "block_number", "transaction_hash", "log_index",
+        "token_give", "amount_give", "token_get", "amount_get",
+        "addr_give", "addr_get", "date"
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    ON CONFLICT ON CONSTRAINT index_trades_on_event_identifier DO NOTHING;
+"""
 async def record_trade(event_name, event):
-    logger = logging.getLogger("contract_events")
-    insert_statement = """INSERT INTO trades
-        (
-            "block_number", "transaction_hash", "log_index",
-            "token_give", "amount_give", "token_get", "amount_get",
-            "addr_give", "addr_get", "date"
-        )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-        ON CONFLICT ON CONSTRAINT index_trades_on_event_identifier DO NOTHING;"""
+    block_number = coerce_to_int(event["blockNumber"])
+    log_index = coerce_to_int(event["logIndex"])
+    date = datetime.fromtimestamp(block_timestamp(App().web3, event["blockNumber"]), tz=None)
 
     insert_args = (
-        event["blockNumber"] if isinstance(event["blockNumber"], int) else Web3.toInt(hexstr=event["blockNumber"]),
+        block_number,
         Web3.toBytes(hexstr=event["transactionHash"]),
-        event["logIndex"] if isinstance(event["logIndex"], int) else Web3.toInt(hexstr=event["logIndex"]),
+        log_index,
         Web3.toBytes(hexstr=event["args"]["tokenGive"]),
         event["args"]["amountGive"],
         Web3.toBytes(hexstr=event["args"]["tokenGet"]),
         event["args"]["amountGet"],
         Web3.toBytes(hexstr=event["args"]["give"]),
         Web3.toBytes(hexstr=event["args"]["get"]),
-        datetime.fromtimestamp(block_timestamp(App().web3, event["blockNumber"]), tz=None)
+        date
     )
 
     async with App().db.acquire_connection() as connection:
-        await connection.execute(insert_statement, *insert_args)
-    logger.debug("recorded trade txid=%s, logidx=%i", event["transactionHash"], event["logIndex"] if isinstance(event["logIndex"], int) else Web3.toInt(hexstr=event["logIndex"]))
+        insert_retval = await connection.execute(INSERT_TRADE_STMT, *insert_args)
+        _, _, did_insert = parse_insert_status(insert_retval)
+
+    if did_insert:
+        logger.debug("recorded trade txid=%s, logidx=%i", event["transactionHash"], log_index)
+
+    return bool(did_insert)
 
 async def record_deposit(event_name, event):
-    logger = logging.getLogger("contract_observer")
-    await record_transfer("DEPOSIT", event)
-    logger.info("recorded deposit txid=%s, logidx=%i", event["transactionHash"], event["logIndex"] if isinstance(event["logIndex"], int) else Web3.toInt(hexstr=event["logIndex"]))
+    did_insert = await record_transfer("DEPOSIT", event)
+    if did_insert:
+        logger.info("recorded deposit txid=%s, logidx=%i", event["transactionHash"], coerce_to_int(event["logIndex"]))
+    return did_insert
 
 async def record_withdraw(event_name, event):
-    logger = logging.getLogger("contract_observer")
-    await record_transfer("WITHDRAW", event)
-    logger.info("recorded deposit txid=%s, logidx=%i", event["transactionHash"], event["logIndex"] if isinstance(event["logIndex"], int) else Web3.toInt(hexstr=event["logIndex"]))
+    did_insert = await record_transfer("WITHDRAW", event)
+    if did_insert:
+        logger.info("recorded withdraw txid=%s, logidx=%i", event["transactionHash"], coerce_to_int(event["logIndex"]))
+    return did_insert
 
-async def record_transfer(transfer_direction, event):
-    insert_statement = """
+INSERT_TRANSFER_STMT = """
     INSERT INTO transfers
-        (
-            "block_number", "transaction_hash", "log_index",
-            "direction", "token", "user", "amount", "balance_after", "date"
-        )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-        ON CONFLICT ON CONSTRAINT index_transfers_on_event_identifier DO NOTHING;"""
+    (
+        "block_number", "transaction_hash", "log_index",
+        "direction", "token", "user", "amount", "balance_after", "date"
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    ON CONFLICT ON CONSTRAINT index_transfers_on_event_identifier DO NOTHING;
+"""
+async def record_transfer(transfer_direction, event):
+    block_number = coerce_to_int(event["blockNumber"])
+    log_index = coerce_to_int(event["logIndex"])
+    date = datetime.fromtimestamp(block_timestamp(App().web3, block_number), tz=None)
 
     insert_args = (
-        event["blockNumber"] if isinstance(event["blockNumber"], int) else Web3.toInt(hexstr=event["blockNumber"]),
+        block_number,
         Web3.toBytes(hexstr=event["transactionHash"]),
-        event["logIndex"] if isinstance(event["logIndex"], int) else Web3.toInt(hexstr=event["logIndex"]),
+        log_index,
         transfer_direction,
         Web3.toBytes(hexstr=event["args"]["token"]),
         Web3.toBytes(hexstr=event["args"]["user"]),
         event["args"]["amount"],
         event["args"]["balance"],
-        datetime.fromtimestamp(block_timestamp(App().web3, event["blockNumber"]), tz=None)
+        date
     )
 
     async with App().db.acquire_connection() as connection:
-        await connection.execute(insert_statement, *insert_args)
+        insert_retval = await connection.execute(INSERT_TRANSFER_STMT, *insert_args)
+        _, _, did_insert = parse_insert_status(insert_retval)
+
+    return bool(did_insert)
 
 def record_cancel(event_name, event):
     print("record_cancel", event)
--- contract_event_utils.py
+++ contract_event_utils.py
@@ -1,3 +1,5 @@
+from web3 import Web3
+
 block_timestamp_cache = {}
 def block_timestamp(web3, block_number):
     global block_timestamp_cache
@@ -5,3 +7,20 @@ def block_timestamp(web3, block_number):
         block_timestamp_cache[block_number] = web3.eth.getBlock(block_number)["timestamp"]
 
     return block_timestamp_cache[block_number]
+
+def coerce_to_int(value):
+    '''
+    Normalizes event values to integers, since WS API returns numbers, and HTTP
+     API returns hexstr.
+    '''
+    if isinstance(value, int):
+        return value
+    return Web3.toInt(hexstr=value)
+
+def parse_insert_status(status_string):
+    '''
+    Returns (command, oid, count) tuple from INSERT status string.
+    cf. https://stackoverflow.com/q/3835314/215024
+    '''
+    command, oid, count = status_string.split(" ")
+    return (command, oid, int(count))

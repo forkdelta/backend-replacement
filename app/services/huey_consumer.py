9febe298981e1b72f2f7564e1bb618717c755f75
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-02-10 23:22:58 +0300

    Use greenlets with huey

--- app/services/huey_consumer.py
+++ app/services/huey_consumer.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all() # cf. http://huey.readthedocs.io/en/latest/troubleshooting.html?highlight=monkey
+
 from ..app import App
 from ..tasks.update_order import update_order_by_signature
 

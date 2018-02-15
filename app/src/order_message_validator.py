0251136ee48d0f96862ee723e3c01d371abc5048
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-02-15 13:55:02 +0300

    [Fixes #29] Correctly coerce exponential notation amountGive, amountGet of an order to int

--- app/src/order_message_validator.py
+++ app/src/order_message_validator.py
@@ -1,8 +1,16 @@
 import cerberus
+from decimal import Decimal
 from eth_utils import is_hex_address, is_0x_prefixed, to_normalized_address
 from web3 import Web3
 
 hexstr_to_bytes = lambda value: Web3.toBytes(hexstr=value)
+
+def str_to_decimal_to_int(value):
+    """
+    Converts a string to Decimal, handling potential exponential notation in
+    the string, then converts to plain int.
+    """
+    return Web3.toInt(Decimal(value))
 
 def validate_0x_prefixed_hex_address(field, value, error):
     if not is_hex_address(value) or not is_0x_prefixed(value):
@@ -11,9 +19,9 @@ def validate_0x_prefixed_hex_address(field, value, error):
 ORDER_MESSAGE_SCHEMA = {
     "contractAddr": { "type": "string", "required": True, "coerce": to_normalized_address, "validator": validate_0x_prefixed_hex_address },
     "tokenGet": { "type": "string", "required": True, "coerce": to_normalized_address, "validator": validate_0x_prefixed_hex_address },
-    "amountGet": { "type": "integer", "required": True, "coerce": Web3.toInt, "min": 0 },
+    "amountGet": { "type": "integer", "required": True, "coerce": str_to_decimal_to_int, "min": 0 },
     "tokenGive": { "type": "string", "required": True, "coerce": to_normalized_address, "validator": validate_0x_prefixed_hex_address },
-    "amountGive": { "type": "integer", "required": True, "coerce": Web3.toInt, "min": 0 },
+    "amountGive": { "type": "integer", "required": True, "coerce": str_to_decimal_to_int, "min": 0 },
     "expires": { "type": "integer", "required": True, "coerce": Web3.toInt, "min": 0 },
     "nonce": { "type": "integer", "required": True, "coerce": Web3.toInt, "min": 0 },
     "user": { "type": "string", "required": True, "coerce": to_normalized_address, "validator": validate_0x_prefixed_hex_address },

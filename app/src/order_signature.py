b22283d09dd396e82ffb3570659399950c50b8ad
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-02-22 01:13:59 +0300

    Clean up deprecation warnings

--- app/src/order_signature.py
+++ app/src/order_signature.py
@@ -15,12 +15,12 @@ def order_signature_valid(message):
     eth_signature_prefix = Web3.toBytes(text="\x19Ethereum Signed Message:\n32")
     hash_bytes = Web3.toBytes(hexstr=make_order_hash(message))
 
-    signature_base = Web3.sha3(Web3.toHex(eth_signature_prefix + hash_bytes))
+    signature_base = Web3.sha3(hexstr=Web3.toHex(eth_signature_prefix + hash_bytes))
 
     recovered_address = ecrecover(
         Web3.toBytes(hexstr=signature_base),
         message["v"],
-        Web3.toInt(message["r"]),
-        Web3.toInt(message["s"]))
+        Web3.toInt(hexstr=Web3.toHex(message["r"])), # Convert from bytes to hex because sending bytes to toInt is deprecated behaviour, apparently
+        Web3.toInt(hexstr=Web3.toHex(message["s"])))
 
     return to_normalized_address(recovered_address) == to_normalized_address(message["user"])

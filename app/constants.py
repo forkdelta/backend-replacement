ace3c971bda6c95f1211e45060cfcf0e6b132ef6
betterdelta <jnanvas@gmail.com>
2018-03-02 12:45:48 +0100

    Missed change

--- app/constants.py
+++ app/constants.py
@@ -10,3 +10,4 @@ ZERO_ADDR_BYTES = Web3.toBytes(hexstr=ZERO_ADDR)
 # We might change our mind about these:
 
 FILTER_ORDERS_UNDER_ETH = 0.001
+ALLOWED_ORIGIN_SUFFIXES = ('forkdelta.com', 'forkdelta.github.io', 'deltabalances.github.io', 'localhost', 'devd.io')

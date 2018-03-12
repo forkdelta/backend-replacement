4c878833d1080eb2b000851a0548ceb78b1c4ac0
mmhh <35501465+betterdelta@users.noreply.github.com>
2018-03-12 19:36:14 +0100

    Add Geth compatibilty (#63)
    * geth compatibilty changes
    
    * Added spaces for better readability
    
    * Two more spaces...

--- app/src/contract_event_utils.py
+++ app/src/contract_event_utils.py
@@ -1,7 +1,16 @@
+from time import time
+
 block_timestamp_cache = {}
 def block_timestamp(web3, block_number):
     global block_timestamp_cache
     if not isinstance(block_number, int) or block_number not in block_timestamp_cache:
-        block_timestamp_cache[block_number] = web3.eth.getBlock(block_number)["timestamp"]
+        block = web3.eth.getBlock(block_number)
+        if block != None:
+            block_timestamp_cache[block_number] = block["timestamp"]
+        else:
+            # Race condition seen with geth where the WS-RPC returns an event from a 
+            # new block, but this block is not yet available through the HTTP-RPC API.
+            # => Assume current time, but don't cache that in block_timestamp_cache.
+            return time()
 
     return block_timestamp_cache[block_number]

03c96e1a1b6d83c17c45cb9b83f925e887828719
mmhh <35501465+betterdelta@users.noreply.github.com>
2018-03-12 19:18:49 +0100

    Added spaces for better readability

--- app/src/contract_event_utils.py
+++ app/src/contract_event_utils.py
@@ -4,9 +4,9 @@ block_timestamp_cache = {}
 def block_timestamp(web3, block_number):
     global block_timestamp_cache
     if not isinstance(block_number, int) or block_number not in block_timestamp_cache:
-        block=web3.eth.getBlock(block_number)
-        if block!=None:
-            block_timestamp_cache[block_number]=block["timestamp"]
+        block = web3.eth.getBlock(block_number)
+        if block != None:
+            block_timestamp_cache[block_number] = block["timestamp"]
         else:
             # Race condition seen with geth where the WS-RPC returns an event from a 
             # new block, but this block is not yet available through the HTTP-RPC API.
